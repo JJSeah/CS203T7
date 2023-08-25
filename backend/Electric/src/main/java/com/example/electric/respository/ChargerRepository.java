c6a6e1dc351e9c0d34927a7d077d668019c96ac1
Jeremy John <jjseah00@gmail.com>
2023-08-26 00:04:06 +0800

    Testing ECS and RDS

---
+++ backend/Electric/src/main/java/com/example/electric/respository/ChargerRepository.java
@@ -0,0 +1,9 @@
+package com.example.electric.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+package com.example.electric.model.Charger;
+
+public interface ChargerRepository extends JpaRepository<Charger, Long> {
+
+}
