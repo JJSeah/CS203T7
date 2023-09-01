ca3b9f328c1bf7708571e4d2ea6d9a8d2b687525
Jess Myn <jessmynchan.2022@scis.smu.edu.sg>
2023-09-01 15:58:32 +0800

    testing git pull

--- frontend/src/components/HidePasswordView.js
+++ frontend/src/components/HidePasswordView.js
@@ -1,13 +1,16 @@
+import React from 'react'
 import { View, Image }  from 'react-native'
 const hidePasswordImagePath = "../../assets/images/hidePassword.png"
 const showPasswordImagePath = "../../assets/images/hidePassword.png"
 
 
-export default HidePasswordView = ( { toggle } ) => {
+export default HidePasswordView = ( { showPassword } ) => {
     return (
-        <View>
-           <Image source={require(Icon)}>
+        <View style = >
 
+
+            <TouchableOpacity onPress = {() => setSeePass}
+           <Image source={require(showPassword ? showPasswordImagePath : hidePasswordImagePath )}>
            </Image>
         </View>
     )
--- frontend/src/view/RegisterScreen.js
+++ frontend/src/view/RegisterScreen.js
@@ -1,10 +1,57 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native'
+import TextField from '../components/TextField'
+import RegisterViewController from '../viewController/RegisterViewController'
 
-export default RegisterScreen = ( ) => {
+export default RegisterScreen = ( { navigation} ) => {
+
+  const {setFirstName, setLastName, setUsername, setEmail, setPassword, setSeePassword, setCheckValidEmail} = RegisterViewController( { navigation } )
+
   return (
-    <View>
-      <Text>This is the register screen</Text>
+    <View style = {styles.container}>
+      <View style = {styles.header}>
+        <Text style={styles.boldText}>Sign Up</Text>
+      </View>
+
+      <View style = {styles.body}>
+        <TextField
+          placeholder = 'First Name'
+          onChangeText={setFirstName}/>
+
+        <TextField
+          placeholder = 'Last Name'
+          onChangeText={setLastName}/>
+         
+        <TextField
+          placeholder = 'Username'
+          onChangeText={setUsername}/>
+
+        <TextField
+          placeholder = 'Email'
+          onChangeText={setEmail}/>  
     </View>
-  );
-};
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1, 
+    backgroundColor: '#fff', 
+  }, 
+  header : {
+    padding : 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    fontSize: 40, 
+    color: 'black', 
+  }, 
+  body:{
+    backgroundColor: '#fff', 
+    fontSize: 30, 
+    padding: 20,
+    marginVertical : 20,
+  }, 
+
+})
