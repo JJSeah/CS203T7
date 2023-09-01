1d5763c107acfbaa34eba95b592eab5282e948e7
Jeremy John <jjseah00@gmail.com>
2023-09-01 19:56:36 +0800

    Finished Records

--- backend/Electric/src/main/java/com/example/electric/controller/RecordController.java
+++ backend/Electric/src/main/java/com/example/electric/controller/RecordController.java
@@ -1,11 +1,45 @@
 package com.example.electric.controller;
 
+import com.example.electric.model.Record;
 import com.example.electric.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/records")
 public class RecordController {
+
+    private final RecordService recordService;
+
     @Autowired
-    private RecordService recordService;
+    public RecordController(RecordService recordService) {
+        this.recordService = recordService;
+    }
+
+    @GetMapping("v1/all")
+    public List<Record> getAllRecords() {
+        return recordService.getAllRecords();
+    }
+
+    @GetMapping("v1/{id}")
+    public Record getRecordById(@PathVariable Long id) {
+        return recordService.getRecordById(id);
+    }
+
+    @PostMapping("v1/")
+    public Record createRecord(@RequestBody Record record) {
+        return recordService.createRecord(record);
+    }
+
+    @PutMapping("v1/{id}")
+    public Record updateRecord(@PathVariable Long id, @RequestBody Record updatedRecord) {
+        return recordService.updateRecord(id, updatedRecord);
+    }
+
+    @DeleteMapping("v1/{id}")
+    public void deleteRecord(@PathVariable Long id) {
+        recordService.deleteRecord(id);
+    }
 }
--- backend/Electric/src/main/java/com/example/electric/service/RecordService.java
+++ backend/Electric/src/main/java/com/example/electric/service/RecordService.java
@@ -1,11 +1,55 @@
 package com.example.electric.service;
 
+import com.example.electric.model.Record;
 import com.example.electric.respository.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecordService {
+
+    private final RecordRepository recordRepository;
+
     @Autowired
-    private RecordRepository recordRepository;
+    public RecordService(RecordRepository recordRepository) {
+        this.recordRepository = recordRepository;
+    }
+
+    public List<Record> getAllRecords() {
+        return recordRepository.findAll();
+    }
+
+    public Record getRecordById(Long id) {
+        Optional<Record> optionalRecord = recordRepository.findById(id);
+        return optionalRecord.orElse(null);
+    }
+
+    public Record createRecord(Record record) {
+        return recordRepository.save(record);
+    }
+
+    public Record updateRecord(Long id, Record updatedRecord) {
+        Optional<Record> optionalRecord = recordRepository.findById(id);
+        if (optionalRecord.isPresent()) {
+            Record record = optionalRecord.get();
+            // Update the record fields as needed
+            record.setOwner(updatedRecord.getOwner());
+            record.setCar(updatedRecord.getCar());
+            record.setDuration(updatedRecord.getDuration());
+            record.setCost(updatedRecord.getCost());
+            record.setStation(updatedRecord.getStation());
+            record.setStartTime(updatedRecord.getStartTime());
+            record.setEndTime(updatedRecord.getEndTime());
+            record.setDate(updatedRecord.getDate());
+            return recordRepository.save(record);
+        } else {
+            return null; // Record not found
+        }
+    }
+
+    public void deleteRecord(Long id) {
+        recordRepository.deleteById(id);
+    }
 }
