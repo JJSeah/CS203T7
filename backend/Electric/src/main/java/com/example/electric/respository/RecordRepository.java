7a78510c4052daee906a8bfbc9cd70d86e987219
dang <truonghaidang01@gmail.com>
2023-09-01 16:52:31 +0800

    Add imports for RecordRepository

--- backend/Electric/src/main/java/com/example/electric/respository/RecordRepository.java
+++ backend/Electric/src/main/java/com/example/electric/respository/RecordRepository.java
@@ -1,7 +1,10 @@
 package com.example.electric.respository;
 
+import com.example.electric.model.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
 }
