e1410d576a6ede1142bc91aa1ac427c90e8c46d5
Leong Zhe Cheng <zhecheng555@gmail.com>
2023-09-04 15:39:31 +0800

    trying out map view in react

--- frontend/src/view/MapScreen.js
+++ frontend/src/view/MapScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Text, View, Button } from 'react-native'
+import MapView from 'react-native-maps'
 
 export default MapScreen = ( ) => {
   return (
     <View>
-      <Text>This is the map screen</Text>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <Text>This is the lol screen</Text>
     </View>
   );
 };
