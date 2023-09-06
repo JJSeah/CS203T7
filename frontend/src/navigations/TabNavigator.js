5dd1e30371de0067b82e20e6473cfd65348235ad
Leong Zhe Cheng <zhecheng555@gmail.com>
2023-09-06 18:26:36 +0800

    changed icon for tab navigator

--- frontend/src/navigations/TabNavigator.js
+++ frontend/src/navigations/TabNavigator.js
@@ -3,47 +3,48 @@ import HomeScreen from '../view/HomeScreen';
 import MapScreen from '../view/MapScreen';
 import HistoryScreen from '../view/HistoryScreen';
 import SettingScreen from '../view/SettingScreen';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 
 const screenOptions = (route, color) => {
   let iconName;
 
-  switch(route.name) {
-    case "HomeScreen":
-      iconName = "home";
+  switch (route.name) {
+    case 'HomeScreen':
+      iconName = 'home';
       break;
-    case "MapScreen":
-      iconName = "appstore-o";
+    case 'MapScreen':
+      iconName = 'appstore-o';
       break;
-    case "HistoryScreen":
-      iconName = "appstore-o";
+    case 'HistoryScreen':
+      iconName = 'folder1';
       break;
-    case "SettingsScreen":
-      iconName = "folder";
+    case 'SettingsScreen':
+      iconName = 'folder1';
       break;
     default:
       break;
   }
 
-  // return <Icon name={iconName} color={color} size={24}/>;
-}
+  return <Icon name={iconName} color={color} size={24} />;
+};
+
 
 export default MyTabs = () => {
 
-
   return (
-    <Tab.Navigator 
-      screenOptions={
-        {
-          headershown: false
-        }
-      }
-    >
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="MapScreen" component={MapScreen} />
-      <Tab.Screen name="HistoryScreen" component={HistoryScreen} />
-      <Tab.Screen name="SettingsScreen" component={SettingScreen} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color}) => screenOptions(route, color),
+      })}
+      >
+        {}
+        <Tab.Screen name="HomeScreen" component={HomeScreen} />
+        <Tab.Screen name="MapScreen" component={MapScreen} />
+        <Tab.Screen name="HistoryScreen" component={HistoryScreen} />
+        <Tab.Screen name="SettingsScreen" component={SettingScreen} />
+        {}
     </Tab.Navigator>
   );
 }
