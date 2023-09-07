32f9427fe91083925b925a54e1abe1e59a65df23
Leong Zhe Cheng <zhecheng555@gmail.com>
2023-09-08 00:04:58 +0800

    wrote a short test data in settings screen for justin to reference

--- frontend/src/view/SettingsScreen.js
+++ frontend/src/view/SettingsScreen.js
@@ -1,5 +1,10 @@
 import { Text, View } from 'react-native';
 
+const testDate = {
+  "name": "user",
+  "email": "user@smu.edu.sg"
+}
+
 export default SettingsScreen = () => {
   return (
     <View>
