62df33db3e865e8c6d82239a1a03a2dc9f0fc25f
Justin Goh <justin.goh.2022@smu.edu.sg>
2023-09-10 12:58:56 +0800

    invalid exception & fail validation exception

--- backend/Electric/src/main/java/com/example/electric/exceptionhandling/GlobalExceptionHandler.java
+++ backend/Electric/src/main/java/com/example/electric/exceptionhandling/GlobalExceptionHandler.java
@@ -2,11 +2,23 @@ package com.example.electric.exceptionhandling;
 
 import com.example.electric.dto.ErrorResponse;
 import com.example.electric.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 import com.example.electric.exception.ObjectAlreadyExistsException;
-import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.*;
+
+import org.springframework.http.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +32,32 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleAppointmentNotFound(ObjectNotFoundException e) {
         ErrorResponse response = new ErrorResponse(e.getErrorCode(),e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    
+    //when arguments did not match the type. e.g user id not a number
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public void handleTypeMismatch(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
+    }
+
+    //method that handle failed validation
+    protected ResponseEntity<Object>
+    handleMethodArgumentNotValid(MethodArgumentNotValidException ex, 
+                                HttpHeaders headers,
+                                 HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        String message = "";
+        for (ObjectError objectError : ex.getBindingResult().getAllErrors()){
+            message = message + objectError.getDefaultMessage();
+        }
+        body.put("message", message);
+        body.put("path", request.getDescription(false));
+        return new ResponseEntity<>(body, headers, status);
+
     }
 }
