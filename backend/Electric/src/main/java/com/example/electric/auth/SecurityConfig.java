484935fae5d34bc2c39108fe98779599e7b21f86
Justin Goh <justin.goh.2022@smu.edu.sg>
2023-09-14 20:53:38 +0800

    Resolve encoder and Added authentication

--- backend/Electric/src/main/java/com/example/electric/auth/SecurityConfig.java
+++ backend/Electric/src/main/java/com/example/electric/auth/SecurityConfig.java
@@ -8,9 +8,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +26,10 @@ public class SecurityConfig  {
 
     }
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder)
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder PasswordEncoder)
             throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(noOpPasswordEncoder);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(PasswordEncoder);
         return authenticationManagerBuilder.build();
     }
 
@@ -40,7 +41,57 @@ public class SecurityConfig  {
                 .authorizeRequests()
                 .requestMatchers("/**").permitAll()
                 //authorisation here
-            //    .requestMatchers("/auth/**").permitAll()
+                    //AuthController
+                    .requestMatchers("/auth/**").permitAll()
+
+                    //AppointmentController
+                    .requestMatchers(HttpMethod.GET, "/api/appointment/**", "/api/appointment").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/appointment//station/*").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/appointment").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/appointment/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/appointment/*").permitAll()
+
+                    //carController
+                    .requestMatchers(HttpMethod.GET, "/api/car/**", "/api/car").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/car").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/car/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/car/*").permitAll()
+
+
+                    //cardController
+                    .requestMatchers(HttpMethod.GET, "/api/card/**", "/api/card").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/card").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/card/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/card/*").permitAll()
+
+                    //chargerController
+                    .requestMatchers(HttpMethod.GET, "/api/charger/**", "/api/charger").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/charger").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/charger/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/charger/*").permitAll()
+
+                    //DistanceController
+                    .requestMatchers(HttpMethod.GET, "/api/distance").permitAll()
+
+                    //RecordController
+                    .requestMatchers(HttpMethod.GET, "/api/records/all", "/api/records/**").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/records").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/records/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/records/*").permitAll()
+
+
+                    //Station contoller
+                    .requestMatchers(HttpMethod.GET, "/api/stations/all", "/api/stations/*").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/stations").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/stations/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/stations/*").permitAll()
+
+                    //User contoller
+                    .requestMatchers(HttpMethod.GET, "/api/user/all", "/api/user/*").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/user/").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/user/*").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/api/user/*").permitAll()
+
 
 
                 .anyRequest().authenticated()
@@ -51,10 +102,18 @@ public class SecurityConfig  {
     }
 
 
-    @SuppressWarnings("deprecation")
-    @Bean
-    public NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }
 
+    // @SuppressWarnings("deprecation")
+    // @Bean
+    // public NoOpPasswordEncoder passwordEncoder() {
+    //     return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    // }
+
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
+
