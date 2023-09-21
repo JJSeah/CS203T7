050cb11bd31168a1fdef8134671b811f22a0aa23
Jeremy John <jjseah00@gmail.com>
2023-09-21 11:10:42 +0800

    added api summary and description

--- backend/Electric/src/main/java/com/example/electric/controller/CardController.java
+++ backend/Electric/src/main/java/com/example/electric/controller/CardController.java
@@ -4,6 +4,7 @@ import com.example.electric.error.ErrorCode;
 import com.example.electric.exception.ObjectNotFoundException;
 import com.example.electric.model.Card;
 import com.example.electric.service.CardService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,7 @@ public class CardController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get Card", description = "Get Card using ID",tags = {"Card"})
     public Optional<Card> getCardById(@PathVariable("id") long id) {
         if (!cardService.getCardById(id).isPresent()) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
@@ -30,6 +32,7 @@ public class CardController {
     }
 
     @GetMapping("/user/{userId}")
+    @Operation(summary = "Get User's Card", description = "Get a list of User's Card from UserID",tags = {"Card"})
     public Optional<Card> getCardByUser(@PathVariable("userId") long userId) {
         if (!cardService.getCardById(userId).isPresent()) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
@@ -38,11 +41,13 @@ public class CardController {
     }
 
     @PostMapping
+    @Operation(summary = "Add Card", description = "Add Card using ID",tags = {"Card"})
     public Card addCard(@RequestBody Card card) {
         return cardService.addCard(card);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update Card", description = "Update Card using ID",tags = {"Card"})
     public Card updateCard(@RequestBody Card card, long id) {
         if (!cardService.getCardById(id).isPresent()) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
@@ -51,6 +56,7 @@ public class CardController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Card", description = "Delete Card using ID",tags = {"Card"})
     public void deleteCard(@PathVariable("id") long id) {
         if (!cardService.getCardById(id).isPresent()) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
--- backend/Electric/src/main/java/com/example/electric/controller/RecordController.java
+++ backend/Electric/src/main/java/com/example/electric/controller/RecordController.java
@@ -4,6 +4,7 @@ import com.example.electric.error.ErrorCode;
 import com.example.electric.exception.ObjectNotFoundException;
 import com.example.electric.model.Record;
 import com.example.electric.service.RecordService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +22,13 @@ public class RecordController {
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get All Records", description = "Get All Records",tags = {"Record"})
     public List<Record> getAllRecords() {
         return recordService.getAllRecords();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get Records", description = "Get Record by ID",tags = {"Record"})
     public Record getRecordById(@PathVariable Long id) {
         if (recordService.getRecordById(id) == null) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
@@ -34,16 +37,19 @@ public class RecordController {
     }
 
     @GetMapping("/user/{userId}")
+    @Operation(summary = "Get User's Records", description = "Get User's Records by UserID",tags = {"Record"})
     public List<Record> getAllRecordsByUser(@PathVariable Long userId) {
         return recordService.getAllRecordsByUser(userId);
     }
 
     @PostMapping
+    @Operation(summary = "Create Records", description = "Create Records",tags = {"Record"})
     public Record createRecord(@RequestBody Record record) {
         return recordService.createRecord(record);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update Records", description = "Update Records",tags = {"Record"})
     public Record updateRecord(@PathVariable Long id, @RequestBody Record updatedRecord) {
         if (recordService.getRecordById(id) == null) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
@@ -52,6 +58,7 @@ public class RecordController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Records", description = "Delete Records",tags = {"Record"})
     public void deleteRecord(@PathVariable Long id) {
         if (recordService.getRecordById(id) == null) {
             throw new ObjectNotFoundException(ErrorCode.E1002);
