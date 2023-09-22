caf2b05fbfb9e3bd61af3f7e2e8ea852def28e9b
Justin Goh <justin.goh.2022@smu.edu.sg>
2023-09-22 23:03:29 +0800

    modified closestStation screen

--- frontend/src/view/ClosestStationView.js
+++ frontend/src/view/ClosestStationView.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect } from 'react';
-import { Button, Text, View, StyleSheet, ActivityIndicator} from 'react-native';
+import { Button, Text, View, StyleSheet, ActivityIndicator, SafeAreaView } from 'react-native';
 import AutomateBookingScreenViewController from '../viewController/AutomateBookingScreenViewController';
 import { UserContext } from '../model/User';
 import MapView, { Marker } from 'react-native-maps';
 import UpcomingAppointmentView from './UpcomingAppointmentView';
+import { styles } from "../components/Design"
 
 export default ClosestStationView = () => {
 
@@ -11,33 +12,39 @@ export default ClosestStationView = () => {
 
     return (
         <View>
-            {/* <MapView style={styles.map}>
-                <Marker 
-                    coordinate={{latitude: closestStation.latitude, longitude: closestStation.longitude}}
+            <MapView style={styles.map}>
+                <Marker
+                    coordinate={{ latitude: closestStation.latitude, longitude: closestStation.longitude }}
                 />
-            </MapView> */}
-            <Text>The closest station is {closestStation.name}</Text>
-            <Text>The closest station id is {closestStation.id}</Text>
-            <Text>The address is {closestStation.address}</Text>
-            <Text>The latitude is {closestStation.latitude}</Text>
-            <Text>The longitude is {closestStation.longitude}</Text>
+            </MapView>
+
+            <View style={{ ...styles.boxContainer, borderTopLeftRadius: 50, borderTopRightRadius: 50, margin: 25, marginBottom:5, padding:30 }}>
+            <Text style= {{...styles.header, color: 'black', margin:0}}>{closestStation.name}</Text>
+            <Text > {closestStation.address}</Text>
+
+            <Text >Station_ID:{closestStation.id}</Text>
+
+            <Text >The latitude is {closestStation.latitude}</Text>
+            <Text >The longitude is {closestStation.longitude}</Text>
+            </View>
 
             <View>
-                {
-                    (upcomingAppointment === null) ?  
-                        <ActivityIndicator/> : 
-                        <UpcomingAppointmentView/>
-                }
+                {/* {
+                        (upcomingAppointment === null) ?
+                            <ActivityIndicator /> :
+                            <UpcomingAppointmentView />
+                    } */}
             </View>
+
         </View>
     );
 }
 
-const styles = StyleSheet.create({
+// const styles = StyleSheet.create({
 
 
-    map: {
-      width: '100%',
-      height: '100%'
-    },
-  });
+//     map: {
+//       width: '100%',
+//       height: '100%'
+//     },
+//   });
