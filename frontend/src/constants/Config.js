08a4e0035450b3b765d106ab5a19dc463be7774a
Justin Dalva Wicent <142854262+WiceKiwi@users.noreply.github.com>
2023-09-25 19:36:05 +0800

    Components for car details in homescreen

--- frontend/src/constants/Config.js
+++ frontend/src/constants/Config.js
@@ -5,9 +5,9 @@
 // Justin PSR
 // export const BASE_URL = "http://10.85.10.106:8080"
 //Justin SMU
-// export const BASE_URL = "http://10.119.155.250:8080"
+export const BASE_URL = "http://10.119.155.250:8080"
 
 // export const BASE_URL = "http://192.168.18.51:8080"
 // for ios
-export const BASE_URL = "http://localhost:8080"
+// export const BASE_URL = "http://localhost:8080"
 
