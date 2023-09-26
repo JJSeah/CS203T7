be38afc186f0351bd3272b9dc83247e387d9939b
Justin Goh <justin.goh.2022@smu.edu.sg>
2023-09-26 08:56:16 +0800

    udpated CarService

---
+++ frontend/src/view/GetHelpScreen.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Text, View } from 'react-native';
+import { UserContext } from '../model/User';
+
+export default GetHelpScreen = () => {
+
+    const { userData } = useContext(UserContext);
+
+  return (
+    <View>
+      <Text>{userData.name}</Text>
+      <Text>{userData.email}</Text>
+      <Text>This is GetHelpScreen screen</Text>
+    </View>
+  );
+}
---
+++ frontend/src/view/VehicleInformationScreen.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Text, View } from 'react-native';
+import { UserContext } from '../model/User';
+
+export default VehicleInformationScreen = () => {
+
+  const { userCars } = useContext(UserContext);
+
+  return (
+    <View>
+
+      { userCars.map(car => {
+        return (
+          <View key={car.id}>
+            <Text>{car.nickname}</Text>
+            <Text>{car.batteryCapacity}</Text>
+          </View>
+        )
+      })}
+    </View>
+  );
+}
