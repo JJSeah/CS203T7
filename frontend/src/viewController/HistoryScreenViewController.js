d8d3f8e10702f9ef03c1b1f8924c8ec0f6259043
Jess Myn <jessmynchan.2022@scis.smu.edu.sg>
2023-10-08 02:14:21 -0700

    updated map

--- frontend/src/viewController/HistoryScreenViewController.js
+++ frontend/src/viewController/HistoryScreenViewController.js
@@ -22,5 +22,4 @@ export default HistoryViewController = ( { navigation } ) => {
         filteredRecords, 
         setFilteredRecords,
     };
-
 }
--- frontend/src/viewController/RegisterScreenViewController.js
+++ frontend/src/viewController/RegisterScreenViewController.js
@@ -17,5 +17,4 @@ export default RegisterViewController = ( { navigation } ) => {
         setIsReady,
         signUpButtonPressed,
     };
-
 }
