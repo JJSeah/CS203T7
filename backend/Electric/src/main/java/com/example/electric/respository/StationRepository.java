d59922d25ee0305c32c1b13f3b85cb9368a75a15
dang <truonghaidang01@gmail.com>
2023-10-19 13:06:17 +0800

    Return charger information for getStationInfo method

--- backend/Electric/src/main/java/com/example/electric/respository/StationRepository.java
+++ backend/Electric/src/main/java/com/example/electric/respository/StationRepository.java
@@ -1,9 +1,11 @@
 package com.example.electric.respository;
 
+import com.example.electric.model.Charger;
 import com.example.electric.model.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
