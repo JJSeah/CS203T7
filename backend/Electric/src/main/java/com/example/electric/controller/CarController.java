d82c5c4dce87260c89f77e2f1fe202c4453e96e8
dang <truonghaidang01@gmail.com>
2023-11-04 18:46:12 +0800

    implement CarControllerTest

--- backend/Electric/src/main/java/com/example/electric/controller/CarController.java
+++ backend/Electric/src/main/java/com/example/electric/controller/CarController.java
@@ -8,6 +8,8 @@ import com.example.electric.service.CarService;
 import com.example.electric.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
